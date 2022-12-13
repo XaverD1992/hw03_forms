39e2bcc79bfe9cc40205e65c13d1950b24161270
Владислав Суворов <gordik_08@mail.ru>
2022-12-13 15:11:30 +0300

    second

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('auth/', include('users.urls', namespace = 'users')),
+    path('auth/', include('users.urls', namespace='users')),
     path('about/', include('about.urls', namespace='about')),
     path('', include('posts.urls', namespace='posts')),
     path('admin/', admin.site.urls),
